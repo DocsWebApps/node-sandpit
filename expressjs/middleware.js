93c7b087f89ef809e7833b0cf7093b733620bab0
Dave Collier <lardelbow@gmail.com>
2015-03-16 22:46:07 +0000

    Level 4 - 16/03/2015

--- expressjs/middleware.js
+++ expressjs/middleware.js
@@ -7,16 +7,15 @@
 var express=require('express'),
     app=express(),
     port=8888,
-    blocks=['Fixed','Movable','Rotating'];
-//  blocks=[{name: 'Fixed', description: 'A fixed thing'},{name: 'Movable', description: 'Can be moved'},{name: 'Rotating', descirption: 'A spining thing'}];    
-// Need to refactor app.get('/blocks')
+    blocks={'Fixed':'A fixed thing','Movable':'Can be moved','Rotating':'A spining thing'},
+    locations={'Fixed':'Static','Movable':'Moving','Rotating':'Spinning'};    
 
 // Use our own middleware layer, defined in the file middleware_logger.js
 var logger=require('./middleware_logger.js');
 app.use(logger);
 
 // You can even delcare the function in the callback
-app.use(function(request, response,next){
+app.use(function(request,response,next) {
   var stream=process.stdout;
   stream.write('Second middleware layer\n');
   next();
@@ -28,13 +27,44 @@ app.use(function(request, response,next){
 app.use(express.static('../app'));
 app.use(express.static('../bower_components'));
 
-app.get('/blocks', function(request, response) {
-  // Use request.query to get query string paramters - http://localhost:888/blocks?limit=1
-  console.log(request.query.limit);
-  if(request.query.limit>=0)  {
-    response.json(blocks.slice(0,request.query.limit));
+// Use middleware to look for variations on parameters
+// Fixed, fixed, FiXed,fIxEd for example
+// Put the param name back into the request object
+// so that we can use it in multiple routes 
+app.param('name', function(request, response, next) {
+  var name=request.params.name;
+  request.paramName=name[0].toUpperCase()+name.slice(1).toLowerCase();
+  next();
+});
+
+// Dynamic routes, similar to ruby :-) - request.params.{param}
+app.get('/blocks/:name', function(request, response){
+  var description=blocks[request.paramName];
+  if(description) {
+    response.json(description);
   } else {
-    response.json(blocks);
+    response.status(404).json(request.paramName+' can not be found');
+  }
+});
+
+app.get('/locations/:name', function(request, response) {
+  var location=locations[request.paramName];
+  if(location) {
+    response.json(location);
+  } else {
+    response.status(404).json(location+' can not be found');
+  }
+});
+
+// Query parameters can be used - request.query.{param}
+app.get('/blocks', function(request, response) {
+  var blocksArray=Object.keys(blocks);
+  // Use request.query to get query string paramters - http://localhost:888/blocks?limit=1
+  //console.log(request.query.limit);
+  if(request.query.limit>=0)  {
+    response.json(blocksArray.slice(0,request.query.limit));
+  } else {
+    response.json(blocksArray);
   }
 });
 
